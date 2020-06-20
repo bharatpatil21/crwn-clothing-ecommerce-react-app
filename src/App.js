9b34249b65e9a87ae02f6658468c5089e42a4442
bharat_patil <patilbharat09@gmail.com>
2020-06-20 08:27:30 +0530

    added user in database and update state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/signin-and-signup/signin-and-signup.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends Component {
   constructor() {
@@ -20,8 +20,16 @@ class App extends Component {
   unSubcribeFromAuth = null;
 
   componentDidMount() {
-    this.unSubcribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unSubcribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({ currentUser: {
+            id: snapShot.id,
+            ...snapShot.data()
+          } })
+        })
+      }
     });
   }
 
@@ -32,7 +40,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD1pp85WV08Qp4hkh2UFHX3WUu32nnO9gw",
@@ -9,7 +9,27 @@ const firebaseConfig = {
   projectId: "ecommerce-clothing-react-app",
   storageBucket: "ecommerce-clothing-react-app.appspot.com",
   messagingSenderId: "341311325274",
-  appId: "1:341311325274:web:3d6ece513ddfd846cf0463"
+  appId: "1:341311325274:web:3d6ece513ddfd846cf0463",
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -18,7 +38,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
